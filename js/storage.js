1a858698783b0d21245fda7d737955824202e5bc
Joana Marinelli <marinellijoana1@gmail.com>
2026-04-14 11:22:55 -0300

    Corrige errores en la función saveToLocalStorage y mejora la gestión del carrito

--- js/storage.js
+++ js/storage.js
@@ -9,3 +9,23 @@ export function getFromLocalStorege() {
     const data = localStorage.getItem(STOREAGE_KEY);
     return data ? JSON.parse(data) : [];
 }
+
+export function saveToLocalStorage(producto, cantidad) {
+    
+    let  cart = getFromLocalStorage();
+    const existe = cart.find(p => p.id === producto.id);
+   
+   if (existe) {
+    existe.cantidad += cantidad;
+} else {
+    cart.push({
+      id: producto.id,
+      title: producto.title,
+      price: producto.price,
+      thumbnail: producto.thumbnail,
+      cantidad: cantidad
+    });
+ }
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(cart));
+}
